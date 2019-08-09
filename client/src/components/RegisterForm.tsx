a2d35a9c86650cd15d1ea7d04e1d9c255b2260cf
TheIllusiveC4 <gasingdong@gmail.com>
2019-08-09 09:05:04 -0700

    add form validation using yup

--- client/src/components/RegisterForm.tsx
+++ client/src/components/RegisterForm.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { FormikProps, Form, Field, withFormik } from 'formik';
+import * as Yup from 'yup';
 
 interface RegisterValues {
   name: string;
@@ -13,9 +14,14 @@ interface RegisterProps {
 
 class RegisterForm extends React.Component<FormikProps<RegisterValues>, {}> {
   public render(): React.ReactElement {
+    const { touched, errors } = this.props;
     return (
       <Form className="register-form">
+        {touched.name && errors.name && <p className="error">{errors.name}</p>}
         <Field type="name" name="name" placeholder="Name" />
+        {touched.password && errors.password && (
+          <p className="error">{errors.password}</p>
+        )}
         <Field type="password" name="password" placeholder="Password" />
         <button type="submit">Register</button>
       </Form>
@@ -33,6 +39,13 @@ const FormikRegisterForm = withFormik<RegisterProps, RegisterValues>({
       password: initialPassword || '',
     };
   },
+
+  validationSchema: Yup.object().shape({
+    name: Yup.string().required('Please enter a name'),
+    password: Yup.string()
+      .min(8, 'Password must be at least 8 characters')
+      .required('Please enter a password'),
+  }),
 
   async handleSubmit(values, { setStatus }): Promise<void> {
     console.log('Submitted: ', values);
